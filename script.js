3aef39335bd1844cb7cbd951a2f84face0ecdd5e
heba sabri <heba.sabri.183@gmail.com>
2024-09-29 22:49:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let quoteArea = document.querySelector("#quote");
+let copyBTN = document.querySelector("button");
+
+let rand = Math.ceil(Math.random()*30);
+// console.log(rand)
+
+fetch(`https://dummyjson.com/quotes/${rand}`).then((res)=>{
+    return res.json();
+}).then((data)=>{
+    console.log(data);
+    quoteArea.innerHTML=`" ${data.quote} " -${data.author}`;
+})
+
+
+copyBTN.addEventListener('click',()=>{
+    quoteArea.select();
+    document.execCommand("copy");
+})
